4c955dfebe41897c4e9ac3ebd2b70469b52cfc37
0v1se <dev@0v1se.com>
2018-04-06 15:34:12 +0300

    0.1.4 spring 5.0.4

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,9 +1,9 @@
 object Versions {
   val scala = "2.12.5"
   val slf4j = "1.7.25"
-  val reactor = "3.1.6.RELEASE"
-  val reactorNetty = "0.7.6.RELEASE"
-  val spring = "5.0.5.RELEASE"
+  val reactor = "3.1.5.RELEASE"
+  val reactorNetty = "0.7.5.RELEASE"
+  val spring = "5.0.4.RELEASE"
   val cats = "1.1.0"
   val jackson = "2.9.4"
   val scalatest = "3.0.4"
