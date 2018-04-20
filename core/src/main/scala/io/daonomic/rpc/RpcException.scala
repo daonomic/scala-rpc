e62d6d1f47cce42569d4606097e1edcf5a66628a
0v1se <dev@0v1se.com>
2018-04-20 14:25:09 +0300

    better error message

--- core/src/main/scala/io/daonomic/rpc/RpcException.scala
+++ core/src/main/scala/io/daonomic/rpc/RpcException.scala
@@ -2,4 +2,4 @@ package io.daonomic.rpc
 
 import io.daonomic.rpc.domain.Error
 
-class RpcException(val error: Error) extends Exception(s"${error.code}: ${error.message}")
+class RpcException(val error: Error) extends Exception(s"${error.code}: ${error.fullMessage}")
--- domain/src/main/scala/io/daonomic/rpc/domain/Error.scala
+++ domain/src/main/scala/io/daonomic/rpc/domain/Error.scala
@@ -1,6 +1,8 @@
 package io.daonomic.rpc.domain
 
-case class Error(code: Int, message: String, data: Option[String] = None)
+case class Error(code: Int, message: String, data: Option[String] = None) {
+  def fullMessage: String = message + data.map(": " + _).getOrElse("")
+}
 
 object Error {
   val default: Error = new Error(0, "No result received")
