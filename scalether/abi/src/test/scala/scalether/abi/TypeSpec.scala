426e5defd5e1899ed07c83f34916b5e79752edcf
0v1se <dev@0v1se.com>
2018-05-18 16:53:05 +0300

    Type.apply(String) added - for constructing Type from string representation

--- scalether/abi/src/test/scala/scalether/abi/TypeSpec.scala
+++ scalether/abi/src/test/scala/scalether/abi/TypeSpec.scala
@@ -7,34 +7,34 @@ import scalether.abi.tuple.TupleType
 
 class TypeSpec extends FlatSpec {
   "Type" should "recognize simple types" in {
-    assert(Type.fromString("string") == StringType)
-    assert(Type.fromString("bool") == BoolType)
-    assert(Type.fromString("address") == AddressType)
-    assert(Type.fromString("bytes") == BytesType)
+    assert(Type("string") == StringType)
+    assert(Type("bool") == BoolType)
+    assert(Type("address") == AddressType)
+    assert(Type("bytes") == BytesType)
   }
 
   val bits: Gen[Short] = Gen.choose[Short](1, 32)
 
   it should "recognize uint types" in {
     for(i <- 1 to 32) {
-      assert(Type.fromString("uint" + (i * 8)) == UintType((i * 8).toShort))
+      assert(Type("uint" + (i * 8)) == UintType((i * 8).toShort))
     }
   }
 
   it should "recognize int types" in {
     for(i <- 1 to 32) {
-      assert(Type.fromString("int" + (i * 8)) == IntType((i * 8).toShort))
+      assert(Type("int" + (i * 8)) == IntType((i * 8).toShort))
     }
   }
 
   it should "recognize bytes types" in {
     for(i <- 1 to 32) {
-      assert(Type.fromString("bytes" + (i * 8)) == FixedBytesType((i * 8).toShort))
+      assert(Type("bytes" + (i * 8)) == FixedBytesType((i * 8).toShort))
     }
   }
 
   it should "recognize var array types" in {
-    val result = Type.fromString("uint[]")
+    val result = Type("uint[]")
     assert(result.isInstanceOf[VarArrayType[_]])
     assert(result.dynamic)
     assert(result.asInstanceOf[VarArrayType[_]].`type` == Uint256Type)
@@ -42,7 +42,7 @@ class TypeSpec extends FlatSpec {
   }
 
   it should "recognize fixed array types" in {
-    val result = Type.fromString("uint[5]")
+    val result = Type("uint[5]")
     assert(result.isInstanceOf[FixArrayType[_]])
     assert(!result.dynamic)
     assert(result.asInstanceOf[FixArrayType[_]].`type` == Uint256Type)
@@ -52,8 +52,8 @@ class TypeSpec extends FlatSpec {
 
   it should "recognize tuples" in {
     for(i <- 0 to 22) {
-      val types = for(j <- 1 to i) yield "bool"
-      val result = Type.fromString(s"(${types.mkString(",")})")
+      val types = for(_ <- 1 to i) yield "bool"
+      val result = Type(s"(${types.mkString(",")})")
       assert(result.isInstanceOf[TupleType[_]])
       assert(result.asInstanceOf[TupleType[_]].types.size == i)
       for(j <- 0 until i) {
