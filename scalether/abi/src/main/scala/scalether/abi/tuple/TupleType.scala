49813cdbde0e2c05e92358c8fbe7a080107c8b38
0v1se <dev@0v1se.com>
2018-05-18 16:34:24 +0300

    Type.apply(String) added - for constructing Type from string representation

--- scalether/abi/src/main/scala/scalether/abi/tuple/TupleType.scala
+++ scalether/abi/src/main/scala/scalether/abi/tuple/TupleType.scala
@@ -19,28 +19,8 @@ abstract class TupleType[T] extends Type[T] {
 object TupleType {
   def apply(types: List[Type[_]]): TupleType[_] = types.size match {
     case 0 => UnitType
-    case 1 => Tuple1Type(types.head)
-    case 2 => Tuple2Type(types.head, types(1))
-    case 3 => Tuple3Type(types.head, types(1), types(2))
-    case 4 => Tuple4Type(types.head, types(1), types(2), types(3))
-    case 5 => Tuple5Type(types.head, types(1), types(2), types(3), types(4))
-    case 6 => Tuple6Type(types.head, types(1), types(2), types(3), types(4), types(5))
-    case 7 => Tuple7Type(types.head, types(1), types(2), types(3), types(4), types(5), types(6))
-    case 8 => Tuple8Type(types.head, types(1), types(2), types(3), types(4), types(5), types(6), types(7))
-    case 9 => Tuple9Type(types.head, types(1), types(2), types(3), types(4), types(5), types(6), types(7), types(8))
-    case 10 => Tuple10Type(types.head, types(1), types(2), types(3), types(4), types(5), types(6), types(7), types(8), types(9))
-    case 11 => Tuple11Type(types.head, types(1), types(2), types(3), types(4), types(5), types(6), types(7), types(8), types(9), types(10))
-    case 12 => Tuple12Type(types.head, types(1), types(2), types(3), types(4), types(5), types(6), types(7), types(8), types(9), types(10), types(11))
-    case 13 => Tuple13Type(types.head, types(1), types(2), types(3), types(4), types(5), types(6), types(7), types(8), types(9), types(10), types(11), types(12))
-    case 14 => Tuple14Type(types.head, types(1), types(2), types(3), types(4), types(5), types(6), types(7), types(8), types(9), types(10), types(11), types(12), types(13))
-    case 15 => Tuple15Type(types.head, types(1), types(2), types(3), types(4), types(5), types(6), types(7), types(8), types(9), types(10), types(11), types(12), types(13), types(14))
-    case 16 => Tuple16Type(types.head, types(1), types(2), types(3), types(4), types(5), types(6), types(7), types(8), types(9), types(10), types(11), types(12), types(13), types(14), types(15))
-    case 17 => Tuple17Type(types.head, types(1), types(2), types(3), types(4), types(5), types(6), types(7), types(8), types(9), types(10), types(11), types(12), types(13), types(14), types(15), types(16))
-    case 18 => Tuple18Type(types.head, types(1), types(2), types(3), types(4), types(5), types(6), types(7), types(8), types(9), types(10), types(11), types(12), types(13), types(14), types(15), types(16), types(17))
-    case 19 => Tuple19Type(types.head, types(1), types(2), types(3), types(4), types(5), types(6), types(7), types(8), types(9), types(10), types(11), types(12), types(13), types(14), types(15), types(16), types(17), types(18))
-    case 20 => Tuple20Type(types.head, types(1), types(2), types(3), types(4), types(5), types(6), types(7), types(8), types(9), types(10), types(11), types(12), types(13), types(14), types(15), types(16), types(17), types(18), types(19))
-    case 21 => Tuple21Type(types.head, types(1), types(2), types(3), types(4), types(5), types(6), types(7), types(8), types(9), types(10), types(11), types(12), types(13), types(14), types(15), types(16), types(17), types(18), types(19), types(20))
-    case 22 => Tuple22Type(types.head, types(1), types(2), types(3), types(4), types(5), types(6), types(7), types(8), types(9), types(10), types(11), types(12), types(13), types(14), types(15), types(16), types(17), types(18), types(19), types(20), types(21))
+    case i if i > 0 && i <= 22 =>
+      Class.forName(s"scalether.abi.tuple.Tuple${i}Type").getConstructors.head.newInstance(types:_*).asInstanceOf[TupleType[_]]
     case _ => throw new IllegalArgumentException(s"types.size not supported: ${types.size}")
   }
 }
