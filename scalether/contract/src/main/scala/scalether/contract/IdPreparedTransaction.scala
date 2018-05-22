1c4902d81254b9919cdfd6cce1aa99452ea63095
0v1se <dev@0v1se.com>
2018-05-22 13:18:43 +0300

    PreparedTransaction improvement

--- scalether/contract/src/main/scala/scalether/contract/IdPreparedTransaction.scala
+++ scalether/contract/src/main/scala/scalether/contract/IdPreparedTransaction.scala
@@ -6,26 +6,27 @@ import cats.Id
 import io.daonomic.cats.implicits._
 import io.daonomic.rpc.domain.{Binary, Word}
 import scalether.abi.Signature
+import scalether.abi.tuple.TupleType
 import scalether.domain.Address
 import scalether.transaction.IdTransactionSender
 
 class IdPreparedTransaction[O](address: Address,
-                                 signature: Signature[_, O],
-                                 data: Binary,
-                                 sender: IdTransactionSender,
-                                 value: BigInteger,
-                                 gas: BigInteger,
-                                 gasPrice: BigInteger)
-  extends PreparedTransaction[Id, O](address, signature, data, sender, value, gas, gasPrice) {
+                               out: TupleType[O],
+                               data: Binary,
+                               sender: IdTransactionSender,
+                               value: BigInteger,
+                               gas: BigInteger,
+                               gasPrice: BigInteger)
+  extends PreparedTransaction[Id, O](address, out, data, sender, value, gas, gasPrice) {
 
   override def withGas(newGas: BigInteger): IdPreparedTransaction[O] =
-    new IdPreparedTransaction[O](address, signature, data, sender, value, newGas, gasPrice)
+    new IdPreparedTransaction[O](address, out, data, sender, value, newGas, gasPrice)
 
   override def withGasPrice(newGasPrice: BigInteger): IdPreparedTransaction[O] =
-    new IdPreparedTransaction[O](address, signature, data, sender, value, gas, newGasPrice)
+    new IdPreparedTransaction[O](address, out, data, sender, value, gas, newGasPrice)
 
   override def withValue(newValue: BigInteger): IdPreparedTransaction[O] =
-    new IdPreparedTransaction[O](address, signature, data, sender, newValue, gas, gasPrice)
+    new IdPreparedTransaction[O](address, out, data, sender, newValue, gas, gasPrice)
 
   override def call(): O = super.call()
 
@@ -44,5 +45,5 @@ object IdPreparedTransaction {
                   value: BigInteger = null,
                   gas: BigInteger = null,
                   gasPrice: BigInteger = null): IdPreparedTransaction[O] =
-    new IdPreparedTransaction[O](address, signature, signature.encode(in), sender, value, gas, gasPrice)
+    new IdPreparedTransaction[O](address, signature.out, signature.encode(in), sender, value, gas, gasPrice)
 }
--- scalether/contract/src/main/scala/scalether/contract/PreparedTransaction.scala
+++ scalether/contract/src/main/scala/scalether/contract/PreparedTransaction.scala
@@ -6,6 +6,7 @@ import cats.implicits._
 import io.daonomic.cats.MonadThrowable
 import io.daonomic.rpc.domain.{Binary, Word}
 import scalether.abi.Signature
+import scalether.abi.tuple.TupleType
 import scalether.domain.request.Transaction
 import scalether.domain.Address
 import scalether.transaction.TransactionSender
@@ -13,7 +14,7 @@ import scalether.transaction.TransactionSender
 import scala.language.higherKinds
 
 class PreparedTransaction[F[_], O](val address: Address,
-                                   val signature: Signature[_, O],
+                                   val out: TupleType[O],
                                    val data: Binary,
                                    sender: TransactionSender[F],
                                    val value: BigInteger,
@@ -22,17 +23,17 @@ class PreparedTransaction[F[_], O](val address: Address,
                                   (implicit m: MonadThrowable[F]) {
 
   def withGas(newGas: BigInteger): PreparedTransaction[F, O] =
-    new PreparedTransaction[F, O](address, signature, data, sender, value, newGas, gasPrice)
+    new PreparedTransaction[F, O](address, out, data, sender, value, newGas, gasPrice)
 
   def withGasPrice(newGasPrice: BigInteger): PreparedTransaction[F, O] =
-    new PreparedTransaction[F, O](address, signature, data, sender, value, gas, newGasPrice)
+    new PreparedTransaction[F, O](address, out, data, sender, value, gas, newGasPrice)
 
   def withValue(newValue: BigInteger): PreparedTransaction[F, O] =
-    new PreparedTransaction[F, O](address, signature, data, sender, newValue, gas, gasPrice)
+    new PreparedTransaction[F, O](address, out, data, sender, newValue, gas, gasPrice)
 
   def call(): F[O] =
     sender.call(Transaction(to = address, data = data, value = value, gas = gas, gasPrice = gasPrice))
-      .map(binary => signature.decode(binary))
+      .map(binary => out.decode(binary, 0).value)
 
   def execute(): F[Word] =
     sender.sendTransaction(Transaction(to = address, data = data, value = value, gas = gas, gasPrice = gasPrice))
@@ -53,5 +54,5 @@ object PreparedTransaction {
                         gas: BigInteger = null,
                         gasPrice: BigInteger = null)
                        (implicit m: MonadThrowable[F]): PreparedTransaction[F, O] =
-    new PreparedTransaction[F, O](address, signature, signature.encode(in), sender, value, gas, gasPrice)
+    new PreparedTransaction[F, O](address, signature.out, signature.encode(in), sender, value, gas, gasPrice)
 }
