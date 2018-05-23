264f40f1c3cc1130091a339e344ee97babfe4794
0v1se <dev@0v1se.com>
2018-05-23 11:15:13 +0300

    BoolType uses java Boolean

--- scalether/abi/src/main/scala/scalether/abi/BoolType.scala
+++ scalether/abi/src/main/scala/scalether/abi/BoolType.scala
@@ -4,13 +4,13 @@ import io.daonomic.rpc.domain
 import io.daonomic.rpc.domain.Binary
 import scalether.util.{Bytes, Padding}
 
-object BoolType extends Type[Boolean] {
+object BoolType extends Type[java.lang.Boolean] {
   val FALSE: Array[Byte] = Bytes.filled(32, Bytes.ZERO)
   val TRUE: Array[Byte] = Padding.padLeft(Array(Bytes.ONE), Bytes.ZERO)
 
   def string = "bool"
 
-  def encode(value: Boolean): Binary = if (value) Binary(TRUE) else Binary(FALSE)
+  def encode(value: java.lang.Boolean): Binary = if (value) Binary(TRUE) else Binary(FALSE)
 
-  def decode(data: domain.Bytes, offset: Int): Decoded[Boolean] = Decoded(data.bytes(31) == 0x1, offset + 32)
+  def decode(data: domain.Bytes, offset: Int): Decoded[java.lang.Boolean] = Decoded(data.bytes(31) == 0x1, offset + 32)
 }
