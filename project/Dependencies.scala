4d601775c0b3950270e143d719efe38eafa4c8c0
0v1se <dev@0v1se.com>
2018-08-01 12:05:31 +0300

    update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,25 +2,25 @@ import sbt._
 
 object Dependencies {
   val fullScalaVersion = "2.12.6"
-  val jacksonVersion = "2.9.4"
+  val jacksonVersion = "2.9.6"
 
   val slf4jApi = "org.slf4j" % "slf4j-api" % "1.7.25"
-  val reactor = "io.projectreactor" % "reactor-core" % "3.1.5.RELEASE"
-  val reactorNetty = "io.projectreactor.ipc" % "reactor-netty" % "0.7.5.RELEASE"
-  val cats = "org.typelevel" %% "cats-core" % "1.1.0"
-  val springWebFlux = "org.springframework" % "spring-webflux" % "5.0.4.RELEASE"
-  val scalajHttp = "org.scalaj" %% "scalaj-http" % "2.3.0"
+  val reactor = "io.projectreactor" % "reactor-core" % "3.1.8.RELEASE"
+  val reactorNetty = "io.projectreactor.ipc" % "reactor-netty" % "0.7.8.RELEASE"
+  val cats = "org.typelevel" %% "cats-core" % "1.2.0"
+  val springWebFlux = "org.springframework" % "spring-webflux" % "5.0.8.RELEASE"
+  val scalajHttp = "org.scalaj" %% "scalaj-http" % "2.4.1"
   val sttp = "com.softwaremill.sttp" %% "core" % "1.2.2"
 
   val jackson = "com.fasterxml.jackson.core" % "jackson-databind" % jacksonVersion
   val jacksonScala = "com.fasterxml.jackson.module" %% "jackson-module-scala" % jacksonVersion
 
-  val scalaTest = "org.scalatest" %% "scalatest" % "3.0.4"
-  val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.13.5"
+  val scalaTest = "org.scalatest" %% "scalatest" % "3.0.5"
+  val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.14.0"
   val mockito = "org.mockito" % "mockito-all" % "1.10.19"
   val logbackClassic = "ch.qos.logback" % "logback-classic" % "1.2.3"
 
-  val bouncyCastle = "org.bouncycastle" % "bcprov-jdk15on" % "1.58"
+  val bouncyCastle = "org.bouncycastle" % "bcprov-jdk15on" % "1.60"
   val junit = "junit" % "junit" % "4.11"
 
   val freemarker = "org.freemarker" % "freemarker" % "2.3.23"
