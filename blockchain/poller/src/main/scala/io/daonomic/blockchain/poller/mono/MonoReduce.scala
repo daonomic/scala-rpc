20f252f54f7980f4942ddec1777ed69ec3cd0651
0v1se <dev@0v1se.com>
2018-10-03 15:20:01 +0300

    fix

--- blockchain/poller/src/main/scala/io/daonomic/blockchain/poller/mono/MonoReduce.scala
+++ blockchain/poller/src/main/scala/io/daonomic/blockchain/poller/mono/MonoReduce.scala
@@ -2,26 +2,31 @@ package io.daonomic.blockchain.poller.mono
 
 import org.reactivestreams.{Subscriber, Subscription}
 import reactor.core.publisher.{Mono, MonoSink}
+import reactor.core.scheduler.Schedulers
 
 object MonoReduce {
+  private val scheduler = Schedulers.newSingle("mono-notifier")
+
   def reduce[T, R](r: R, list: Seq[T])(reduce: (R, T) => Mono[R]): Mono[R] = Mono.create { sink =>
     subscribe(r, list, reduce, sink)
   }
 
   def subscribe[T, R](r: R, list: Seq[T], reduce: (R, T) => Mono[R], sink: MonoSink[R]): Unit = list match {
     case _ if list.isEmpty => sink.success(r)
-    case _ => reduce(r, list.head).subscribe(new Subscriber[R] {
-      override def onSubscribe(s: Subscription): Unit = {
-        s.request(1)
-      }
+    case _ => reduce(r, list.head)
+      .subscribeOn(scheduler)
+      .subscribe(new Subscriber[R] {
+        override def onSubscribe(s: Subscription): Unit = {
+          s.request(1)
+        }
 
-      override def onNext(r: R): Unit =
-        subscribe(r, list.tail, reduce, sink)
+        override def onNext(r: R): Unit =
+          subscribe(r, list.tail, reduce, sink)
 
-      override def onError(t: Throwable): Unit =
-        sink.error(t)
+        override def onError(t: Throwable): Unit =
+          sink.error(t)
 
-      override def onComplete(): Unit = {}
-    })
+        override def onComplete(): Unit = {}
+      })
   }
 }
