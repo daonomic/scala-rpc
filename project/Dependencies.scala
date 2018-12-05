23fc7184071226bc24eea20766c21817acdc33bd
0v1se <dev@0v1se.com>
2018-12-05 14:00:51 +0300

    spring 5.1.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,10 +5,10 @@ object Dependencies {
   val jacksonVersion = "2.9.7"
 
   val slf4jApi = "org.slf4j" % "slf4j-api" % "1.7.25"
-  val reactor = "io.projectreactor" % "reactor-core" % "3.2.2.RELEASE"
-  val reactorNetty = "io.projectreactor.netty" % "reactor-netty" % "0.8.2.RELEASE"
+  val reactor = "io.projectreactor" % "reactor-core" % "3.2.3.RELEASE"
+  val reactorNetty = "io.projectreactor.netty" % "reactor-netty" % "0.8.3.RELEASE"
   val cats = "org.typelevel" %% "cats-core" % "1.4.0"
-  val springWebFlux = "org.springframework" % "spring-webflux" % "5.1.2.RELEASE"
+  val springWebFlux = "org.springframework" % "spring-webflux" % "5.1.3.RELEASE"
   val scalajHttp = "org.scalaj" %% "scalaj-http" % "2.4.1"
   val sttp = "com.softwaremill.sttp" %% "core" % "1.4.2"
 
