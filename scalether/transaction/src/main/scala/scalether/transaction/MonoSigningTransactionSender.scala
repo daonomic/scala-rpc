b4413676e0f21d3bb6d4983d0560aa160ffb44aa
0v1se <dev@0v1se.com>
2018-12-19 16:17:06 +0300

    added Synchronizer

--- scalether/transaction/src/main/scala/scalether/transaction/MonoSigningTransactionSender.scala
+++ scalether/transaction/src/main/scala/scalether/transaction/MonoSigningTransactionSender.scala
@@ -7,13 +7,13 @@ import io.daonomic.rpc.domain.Word
 import reactor.core.publisher.Mono
 import scalether.core.MonoEthereum
 import scalether.domain.request.Transaction
-import scalether.sync.MonoSynchronizer
+import scalether.sync.{MonoSynchronizer, SemaphoreMonoSynchronizer}
 
 class MonoSigningTransactionSender(ethereum: MonoEthereum, nonceProvider: MonoNonceProvider, synchronizer: MonoSynchronizer, privateKey: BigInteger, gas: BigInteger, gasPrice: MonoGasPriceProvider)
   extends SigningTransactionSender[Mono](ethereum, nonceProvider, synchronizer, privateKey, gas, gasPrice) with MonoTransactionSender {
 
   def this(ethereum: MonoEthereum, nonceProvider: MonoNonceProvider, privateKey: BigInteger, gas: BigInteger, gasPrice: MonoGasPriceProvider) {
-    this(ethereum, nonceProvider, null, privateKey, gas, gasPrice)
+    this(ethereum, nonceProvider, new SemaphoreMonoSynchronizer(), privateKey, gas, gasPrice)
   }
 
   override def sendTransaction(transaction: Transaction): Mono[Word] =
