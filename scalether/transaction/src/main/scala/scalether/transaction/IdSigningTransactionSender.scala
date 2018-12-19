229459d37126712f05766905b8b1d69f15d361a3
0v1se <dev@0v1se.com>
2018-12-19 16:17:51 +0300

    added Synchronizer

--- scalether/transaction/src/main/scala/scalether/transaction/IdSigningTransactionSender.scala
+++ scalether/transaction/src/main/scala/scalether/transaction/IdSigningTransactionSender.scala
@@ -7,10 +7,14 @@ import io.daonomic.cats.implicits._
 import io.daonomic.rpc.domain.Word
 import scalether.core.IdEthereum
 import scalether.domain.request.Transaction
-import scalether.sync.IdSynchronizer
+import scalether.sync.{IdSynchronizer, SemaphoreMonoSynchronizer}
 
 class IdSigningTransactionSender(ethereum: IdEthereum, nonceProvider: IdNonceProvider, synchronizer: IdSynchronizer, privateKey: BigInteger, gas: BigInteger, gasPrice: IdGasPriceProvider)
   extends SigningTransactionSender[Id](ethereum, nonceProvider, synchronizer, privateKey, gas, gasPrice) with IdTransactionSender {
+
+  def this(ethereum: IdEthereum, nonceProvider: IdNonceProvider, privateKey: BigInteger, gas: BigInteger, gasPrice: IdGasPriceProvider) {
+    this(ethereum, nonceProvider, new SemaphoreMonoSynchronizer(), privateKey, gas, gasPrice)
+  }
 
   override def sendTransaction(transaction: Transaction): Word =
     super.sendTransaction(transaction)
