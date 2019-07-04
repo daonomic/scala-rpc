5bfca1d2ee7b38164310ac5bb44a99bdac54b51e
0v1se <dev@0v1se.com>
2019-07-04 17:27:12 +0300

    deps upgrade

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,13 +2,13 @@ import sbt._
 
 object Dependencies {
   val fullScalaVersion = "2.12.7"
-  val jacksonVersion = "2.9.8"
+  val jacksonVersion = "2.9.9"
 
   val slf4jApi = "org.slf4j" % "slf4j-api" % "1.7.25"
-  val reactor = "io.projectreactor" % "reactor-core" % "3.2.9.RELEASE"
-  val reactorNetty = "io.projectreactor.netty" % "reactor-netty" % "0.8.8.RELEASE"
-  val cats = "org.typelevel" %% "cats-core" % "1.6.0"
-  val springWebFlux = "org.springframework" % "spring-webflux" % "5.1.7.RELEASE"
+  val reactor = "io.projectreactor" % "reactor-core" % "3.2.10.RELEASE"
+  val reactorNetty = "io.projectreactor.netty" % "reactor-netty" % "0.8.9.RELEASE"
+  val cats = "org.typelevel" %% "cats-core" % "1.6.1"
+  val springWebFlux = "org.springframework" % "spring-webflux" % "5.1.8.RELEASE"
   val scalajHttp = "org.scalaj" %% "scalaj-http" % "2.4.1"
   val sttp = "com.softwaremill.sttp" %% "core" % "1.4.2"
 
@@ -20,7 +20,7 @@ object Dependencies {
   val mockito = "org.mockito" % "mockito-all" % "1.10.19"
   val logbackClassic = "ch.qos.logback" % "logback-classic" % "1.2.3"
 
-  val bouncyCastle = "org.bouncycastle" % "bcprov-jdk15on" % "1.60"
+  val bouncyCastle = "org.bouncycastle" % "bcprov-jdk15on" % "1.62"
   val junit = "junit" % "junit" % "4.11"
 
   val freemarker = "org.freemarker" % "freemarker" % "2.3.23"
