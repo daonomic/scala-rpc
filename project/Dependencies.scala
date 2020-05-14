e96cf1ed2d0787f4cc73fa83e3058a3c2ba85e2d
roborox <dev@roborox.org>
2020-05-14 17:45:36 +0300

    upgrade to scala 2.13

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,23 +1,23 @@
 import sbt._
 
 object Dependencies {
-  val fullScalaVersion = "2.12.10"
+  val fullScalaVersion = "2.13.2"
   val jacksonVersion = "2.11.0"
   val jacksonDatabindVersion = "2.11.0"
 
   val slf4jApi = "org.slf4j" % "slf4j-api" % "1.7.28"
   val reactor = "io.projectreactor" % "reactor-core" % "3.3.5.RELEASE"
   val reactorNetty = "io.projectreactor.netty" % "reactor-netty" % "0.9.7.RELEASE"
-  val cats = "org.typelevel" %% "cats-core" % "1.6.1"
+  val cats = "org.typelevel" %% "cats-core" % "2.1.1"
   val springWebFlux = "org.springframework" % "spring-webflux" % "5.2.6.RELEASE"
-  val scalajHttp = "org.scalaj" %% "scalaj-http" % "2.4.1"
-  val sttp = "com.softwaremill.sttp" %% "core" % "1.4.2"
+  val scalajHttp = "org.scalaj" %% "scalaj-http" % "2.4.2"
+  val sttp = "com.softwaremill.sttp" %% "core" % "1.7.2"
 
   val jackson = "com.fasterxml.jackson.core" % "jackson-databind" % jacksonDatabindVersion
   val jacksonScala = "com.fasterxml.jackson.module" %% "jackson-module-scala" % jacksonVersion
 
-  val scalaTest = "org.scalatest" %% "scalatest" % "3.0.5"
-  val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.14.0"
+  val scalaTest = "org.scalatest" %% "scalatest" % "3.1.2"
+  val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.14.3"
   val mockito = "org.mockito" % "mockito-all" % "1.10.19"
   val logbackClassic = "ch.qos.logback" % "logback-classic" % "1.2.3"
 
