481f2d07817316ad1e31b5b255757cf06f8b917a
roborox <dev@roborox.org>
2020-05-14 17:35:12 +0300

    upgrade deps

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,14 +2,14 @@ import sbt._
 
 object Dependencies {
   val fullScalaVersion = "2.12.10"
-  val jacksonVersion = "2.10.2"
-  val jacksonDatabindVersion = "2.10.2"
+  val jacksonVersion = "2.11.0"
+  val jacksonDatabindVersion = "2.11.0"
 
   val slf4jApi = "org.slf4j" % "slf4j-api" % "1.7.28"
-  val reactor = "io.projectreactor" % "reactor-core" % "3.3.2.RELEASE"
-  val reactorNetty = "io.projectreactor.netty" % "reactor-netty" % "0.9.4.RELEASE"
+  val reactor = "io.projectreactor" % "reactor-core" % "3.3.5.RELEASE"
+  val reactorNetty = "io.projectreactor.netty" % "reactor-netty" % "0.9.7.RELEASE"
   val cats = "org.typelevel" %% "cats-core" % "1.6.1"
-  val springWebFlux = "org.springframework" % "spring-webflux" % "5.2.3.RELEASE"
+  val springWebFlux = "org.springframework" % "spring-webflux" % "5.2.6.RELEASE"
   val scalajHttp = "org.scalaj" %% "scalaj-http" % "2.4.1"
   val sttp = "com.softwaremill.sttp" %% "core" % "1.4.2"
 
