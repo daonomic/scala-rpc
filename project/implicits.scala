199a89dd6a7f5af53e0fc6415b1b6369a7a4a198
0v1se <dev@0v1se.com>
2021-04-28 14:09:39 +0300

    0.1.2 publish to github packages

--- project/implicits.scala
+++ project/implicits.scala
@@ -1,15 +1,10 @@
 import Dependencies.{logbackClassic, mockito, scalaCheck, scalaTest}
 import sbt.Keys._
-import sbt.io.{Path, RichFile}
-import sbt.librarymanagement.DependencyBuilders.RepositoryName
-import sbt.librarymanagement.ivy.Credentials
 import sbt.{Project, url}
+import sbtghpackages.GitHubPackagesKeys._
+import sbtghpackages.TokenSource
 
 object implicits {
-
-  val bintrayUser = sbt.settingKey[String]("Bintray user name")
-  val bintrayRepository = sbt.settingKey[String]("Bintray repository name")
-  val bintrayPackage = sbt.settingKey[String]("Bintray package name")
 
   implicit class RichProject(project: Project) {
     def tests(scope: String = "test"): Project = project
@@ -18,50 +13,28 @@ object implicits {
       .settings(libraryDependencies += mockito % scope)
       .settings(libraryDependencies += logbackClassic % scope)
 
-    def publish: Project = project.settings(
-      bintrayUser := "daonomic",
-      bintrayRepository := "maven",
-      bintrayPackage := name.value,
-      credentials += {
-        if (isSnapshot.value) {
-          Credentials((Path.userHome: RichFile) / ".ivy2/nexus.credentials")
-        } else {
-          Credentials((Path.userHome: RichFile) / ".ivy2/bintray.credentials")
-        }
-      },
-      publishTo := {
-        if (isSnapshot.value) {
-          Some((("snapshots": RepositoryName) at "http://10.7.3.6:8081/nexus/content/repositories/snapshots/").withAllowInsecureProtocol(true))
-        } else {
-          Some(("releases": RepositoryName) at s"https://api.bintray.com/maven/${bintrayUser.value}/${bintrayRepository.value}/${bintrayPackage.value}/;publish=1")
-        }
-      }
-    )
-
-    def common: Project = publish.settings(
+    def common: Project = project.settings(
       organization := "io.daonomic.rpc",
-      bintrayPackage := s"scala-rpc-${name.value}",
       licenses += ("MIT", url("http://opensource.org/licenses/MIT")),
-      version := "1.1-SNAPSHOT",
-      scalaVersion := Dependencies.fullScalaVersion
+      version := "0.1.2",
+      scalaVersion := Dependencies.fullScalaVersion,
+      githubOwner := "daonomic",
+      githubRepository := "scala-rpc",
+      githubTokenSource := TokenSource.GitConfig("github.token")
     )
 
     def transport: Project = common
-      .settings(bintrayPackage := s"scala-rpc-${name.value}")
 
     def scalether: Project = common.settings(
-      organization := "io.daonomic.scalether",
-      bintrayPackage := s"scalether-${name.value}"
+      organization := "io.daonomic.scalether"
     )
 
     def bitcoin: Project = common.settings(
-      organization := "io.daonomic.bitcoin.rpc",
-      bintrayPackage := s"bitcoin-${name.value}"
+      organization := "io.daonomic.bitcoin.rpc"
     )
 
     def blockchain: Project = common.settings(
-      organization := "io.daonomic.blockchain",
-      bintrayPackage := s"blockchain-${name.value}"
+      organization := "io.daonomic.blockchain"
     )
   }
 }
